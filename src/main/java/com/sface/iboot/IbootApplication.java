bc41b9c316e83420ae36d700a0d2c3e3e3f7cf9b
hzSFace <lyrbhz@163.com>
2019-05-17 09:31:59 +0800

    创建iboot项目

---
+++ src/main/java/com/sface/iboot/IbootApplication.java
@@ -0,0 +1,13 @@
+package com.sface.iboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbootApplication.class, args);
+    }
+
+}
